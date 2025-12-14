b078e471557f10d736fb8503d5986bd42e4444b6
Arthur-AP131 <arthursantos.ferreira131@gmail.com>
2025-12-14 03:51:40 -0300

    database

---
+++ teste07_database.py
@@ -0,0 +1,30 @@
+import mysql.connector
+
+try:
+    MyDB = mysql.connector.connect(
+        host="localhost",
+        user="root",
+        password="password123",
+        database="frutasDB"
+    )
+
+    if MyDB.is_connected:
+        print("Conexão bem sucedida!!")
+    else:
+        print("Erro na conexão!!")
+
+    cur = MyDB.cursor()
+
+    cur.execute("SELECT * FROM fruta WHERE preço < 10 ")
+
+    for row in cur.fetchall():
+        print(row)
+
+    MyDB.close()
+    
+except mysql.connector.Error as err:
+    print(f'Erro: {err}')
+
+
+
+    
